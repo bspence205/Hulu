0678c9a61811128a52380cc2b053b3693115622e
bspence205 <brianspencer@spencerbrian.com>
2022-03-01 13:52:08 -0800

    updated base tailwindCSS in globalCSS to change background, created HeaderItem component to create and style header icons, imported heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,30 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon, 
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline"
+
+
+import Image from "next/image";
 import React from "react";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
     <header className="">
-      <h1>This is the Header</h1>
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt="Hulu logo"
+      />
     </header>
   );
 }
