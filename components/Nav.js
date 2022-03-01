b538751a11244fda6e9f4aa1a5fdc0504cba751d
bspence205 <brianspencer@spencerbrian.com>
2022-03-01 14:22:11 -0800

    created local env, created utils folder and requestJS to request API from tmbd

---
+++ components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav() {
+  return <nav>
+      <div></div>
+  </nav>;
+}
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import Header from "../components/Header"
+import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -13,10 +14,10 @@ export default function Home() {
         />
       </Head>
 
-      {/* Header */}
       <Header />
 
       {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
